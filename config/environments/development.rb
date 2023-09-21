b4c0e21d748ace748282ee0fb434ee145f4eb3af
Usama-Khaan <usama.khan@xprolabs.com>
2023-09-21 17:59:32 +0500

    [103]-Email is sending to user

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,18 +59,11 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
-  # For email authentication
+  # configuration for mail-catcher
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    user_name: 'usamakhan',
-    password: 'usamakhan@',
-    domain: 'labc@gmail.com',
-    address: 'smtp.gmail.com',
-    port: '587',
-    authentication: :plain,
-    enable_starttls_auto: true
-  }
+  config.action_mailer.smtp_settings = { address: '127.0.0.1', port: 1025 }
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.raise_delivery_errors = false
 
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
