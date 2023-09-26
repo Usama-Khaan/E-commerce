d36ee138045a3679d3da6e832d4814638228ea13
Usama-Khaan <usama.khan@xprolabs.com>
2023-09-26 14:44:23 +0500

    Product: Create table migration is added

---
+++ db/migrate/20230926093331_create_products.rb
@@ -0,0 +1,14 @@
+class CreateProducts < ActiveRecord::Migration[7.0]
+  def change
+    drop_table :products
+    create_table :products do |t|
+      t.string :title
+      t.string :description
+      t.string :picture_url
+      t.integer :price
+      t.integer :product_quantity
+      t.integer :product_stock_quantity
+      t.timestamps
+    end
+  end
+end
