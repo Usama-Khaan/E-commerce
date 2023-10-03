c493830f8815ff0cbf95e07c57e3a4b6590b9751
Usama-Khaan <usama.khan@xprolabs.com>
2023-10-03 17:26:09 +0500

    LineItem: LineItem model is added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_03_115507) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_03_122111) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -45,6 +45,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_03_115507) do
   create_table "carts", force: :cascade do |t|
     t.float "cart_total_price"
     t.integer "cart_total_product_quantity"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "line_items", force: :cascade do |t|
+    t.bigint "product_id", null: false
+    t.bigint "cart_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
