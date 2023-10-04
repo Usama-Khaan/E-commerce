a22a9e4d33f7387117a289dee5ac996a7f857ae4
Usama-Khaan <usama.khan@xprolabs.com>
2023-10-04 13:51:27 +0500

    Product: Dependent destroy is added in model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,6 @@
 class Product < ApplicationRecord
   include PaginationConcern
   has_one_attached :image
-  has_many :line_items
-  has_many :carts, :through => :line_items
+  has_many :line_items, dependent: :destroy
   validates :title, :description, :product_stock_quantity, :price, presence: true
 end
