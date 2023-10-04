090f0ec927493f9902869f3101cb308472e6880f
Usama-Khaan <usama.khan@xprolabs.com>
2023-10-04 13:48:50 +0500

    Cart: Cart resources are added

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'carts/index'
   root 'products#index'
 
   devise_for :users, controllers: {
@@ -9,4 +8,8 @@ Rails.application.routes.draw do
 
   resources :users
   resources :products
+  resources :carts, only: [:show, :destroy] do
+    post 'add/:product_id', to: 'carts#add_to_cart', on: :member, as: 'add_product'
+    delete 'remove/:product_id', to: 'carts#remove_from_cart', on: :member, as: 'remove_product'
+  end
 end
