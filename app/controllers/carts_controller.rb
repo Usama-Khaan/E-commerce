f3a1261da5c0700e7c090a5892cf6798e8a2988f
Usama-Khaan <usama.khan@xprolabs.com>
2023-10-05 14:47:00 +0500

    Cart: Custom and default actions are added in controller

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,3 +1,24 @@
 class CartsController < ApplicationController
-  def index; end
+  include FindProduct
+  before_action :check_user_cart, :find_product, only: %i[add_product remove_product]
+
+  def add_product
+    line_item = @cart.line_items.build(product: @product)
+    redirect_to cart_path(@cart), notice: 'Product added to cart.' if line_item.save
+  end
+
+  def remove_product
+    @cart.products.delete(@product)
+    redirect_to cart_path(@cart), notice: 'Product removed from cart.'
+  end
+
+  def show
+    @cart = current_user.cart
+  end
+
+  private
+
+  def check_user_cart
+    @cart = current_user.cart || current_user.create_cart
+  end
 end
