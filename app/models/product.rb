31a9607b1aaa5080b95663c4dddcefeb6a39e6b6
Usama-Khaan <usama.khan@xprolabs.com>
2023-10-05 14:40:09 +0500

    Product: Add concern FindProduct in controller

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,5 +2,6 @@ class Product < ApplicationRecord
   include PaginationConcern
   has_one_attached :image
   has_many :line_items, dependent: :destroy
+  has_many :carts, through: :line_items
   validates :title, :description, :product_stock_quantity, :price, presence: true
 end
