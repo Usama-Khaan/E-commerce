af914993b9af4e300e3917c2cb594ab03c0a6fab
Usama-Khaan <usama.khan@xprolabs.com>
2023-10-09 18:04:11 +0500

    Cart: Member routes feedback incorporated

--- config/routes.rb
+++ config/routes.rb
@@ -10,8 +10,8 @@ Rails.application.routes.draw do
   resources :products
   resources :carts, except: %i[index new] do
     member do
-      get 'add_product/:id', to: 'carts#add_product', as: 'add_product'
-      delete 'remove_product/:id', to: 'carts#remove_product', as: 'remove_product'
+      get 'add_product', to: 'carts#add_product', as: 'add_product'
+      delete 'remove_product', to: 'carts#remove_product', as: 'remove_product'
       delete 'reset', to: 'carts#reset', as: 'reset'
     end
   end
