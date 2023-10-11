3f9c71046fadfaf5f90eb849a90478565bdc4f6a
Usama-Khaan <usama.khan@xprolabs.com>
2023-10-11 20:49:50 +0500

    Order: Validation add associations are added in model

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,5 @@
 class Order < ApplicationRecord
-  belongs_to :users
-  has_many :line_items
+  belongs_to :user
+  belongs_to :cart
+  validates :email, :country, :first_name, :last_name, :address, :city, :postal_code, :phone_number, presence: true
 end
