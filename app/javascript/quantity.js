fd9d5ac1ca3a741a3537d1989ad73ca925fc54a7
Usama-Khaan <usama.khan@xprolabs.com>
2023-10-11 13:31:09 +0500

    Cart: Total amount of cart item is added on show page

--- app/javascript/quantity.js
+++ app/javascript/quantity.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const quantityInputs = document.querySelectorAll('.quantity-input');
   const priceTags = document.querySelectorAll('.price-tag');
   const increaseOrDecreaseBtn = document.getElementsByClassName('increment-decrement-button');
+  const total_price_element = document.getElementById('total-price');
   const THOUSANDS_SEPARATOR_PATTERN = /\d(?=(\d{3})+\.)/g;
 
   function handleQuantityChange(index, increment) {
@@ -11,16 +12,16 @@ document.addEventListener('DOMContentLoaded', () => {
     } else if (!increment && currentValue > 1) {
       quantityInputs[index].value = currentValue - 1;
     }
-    updateProductPrice(index, quantityInputs, priceTags);
+    updateProductPrice(index);
+    updateTotalPrice();
   }
 
-  function updateProductPrice(index, quantityInputs, priceTags) {
+  function updateProductPrice(index) {
+    debugger
     const quantity = parseInt(quantityInputs[index].value);
     const productPriceElement = priceTags[index].closest('.product-details');
-
     if (productPriceElement) {
       const productPrice = parseFloat(productPriceElement.getAttribute('data-product-price'));
-
       if (!isNaN(productPrice)) {
         const totalPrice = quantity * productPrice;
         priceTags[index].textContent = formatNumberAsCurrency(totalPrice);
@@ -32,18 +33,29 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   }
 
+  function updateTotalPrice() {
+    let totalPrice = 0;
+    for (let i = 0; i < quantityInputs.length; i++) {
+      const quantity = parseInt(quantityInputs[i].value);
+      const productPrice = parseFloat(priceTags[i].textContent.replace(/\$|,/g, ''));
+      if (!isNaN(quantity) && !isNaN(productPrice)) {
+        totalPrice += productPrice;
+      }
+    }
+    total_price_element.textContent = formatNumberAsCurrency(totalPrice);
+  }
+
   function formatNumberAsCurrency(number, currencySymbol = '$') {
     const roundedNumber = number.toFixed(2);
     const formattedNumber = roundedNumber.replace(THOUSANDS_SEPARATOR_PATTERN, '$&,');
     return currencySymbol + formattedNumber;
   }
 
-  for( let i = 0; i < increaseOrDecreaseBtn.length; i++ )
-  {
-    let incOrDec = increaseOrDecreaseBtn[i].dataset['action'] == 'increment' ? true : false
+  for (let i = 0; i < increaseOrDecreaseBtn.length; i++) {
+    let incOrDec = increaseOrDecreaseBtn[i].dataset['action'] == 'increment' ? true : false;
     let index = increaseOrDecreaseBtn[i].dataset['index'];
     increaseOrDecreaseBtn[i].addEventListener('click', () => {
-      handleQuantityChange(index,incOrDec);
+      handleQuantityChange(index, incOrDec);
     });
   }
 });
