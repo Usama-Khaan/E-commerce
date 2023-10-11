2123accf6e1b21eb2b940d76250f3b6a711cd71e
Usama-Khaan <usama.khan@xprolabs.com>
2023-10-11 20:50:41 +0500

    Order: Controller actions added

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,26 @@
 class OrdersController < ApplicationController
+  def show
+    @order = Order.find(params[:id])
+  end
+
+  def new
+    @order = Order.new
+  end
+
+  def create
+    @cart = current_user.cart
+    @order = current_user.orders.build(order_params)
+    @order.cart = @cart
+    if @order.save
+      redirect_to order_path(@order)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:id, :email, :city, :country, :first_name, :last_name, :address, :postal_code, :phone_number)
+  end
 end
