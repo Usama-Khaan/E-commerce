ad63201bcfcbab52dc2bfaa87c0389a855be5652
Usama-Khaan <usama.khan@xprolabs.com>
2023-10-11 13:26:17 +0500

    Line item: Line item quantity column added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_10_102114) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_11_064801) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -56,6 +56,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_10_102114) do
     t.bigint "cart_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "line_items_quantity"
   end
 
   create_table "orders", force: :cascade do |t|
@@ -86,7 +87,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_10_102114) do
     t.string "description"
     t.string "picture_url"
     t.integer "price"
-    t.integer "product_quantity"
     t.integer "product_stock_quantity"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
