f30035e849813a5a67160cb4c0399c0df198800e
Usama-Khaan <usama.khan@xprolabs.com>
2023-10-13 16:57:03 +0500

    Order: Add status column in table and set status value pending

---
+++ db/migrate/20231013113810_add_column_to_order.rb
@@ -0,0 +1,5 @@
+class AddColumnToOrder < ActiveRecord::Migration[7.0]
+  def change
+    add_column :orders, :status, :string, default: 'pending'
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_11_115516) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_13_113810) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -72,6 +72,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_11_115516) do
     t.datetime "updated_at", null: false
     t.bigint "user_id"
     t.bigint "cart_id"
+    t.string "status", default: "pending"
     t.index ["cart_id"], name: "index_orders_on_cart_id"
     t.index ["email"], name: "index_orders_on_email", unique: true
     t.index ["user_id"], name: "index_orders_on_user_id"
