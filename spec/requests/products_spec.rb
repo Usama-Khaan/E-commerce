482051ec00e1e2c7e73c76e09ae72d23f9de1681
Usama-Khaan <usama.khan@xprolabs.com>
2023-10-18 17:18:51 +0500

    Product: Errors are resolved for api product test

--- spec/requests/products_spec.rb
+++ spec/requests/products_spec.rb
@@ -2,8 +2,12 @@ require 'rails_helper'
 
 RSpec.describe "Api::V1::Products", type: :request do
   describe "GET /index" do
+    before do
+      allow_any_instance_of(Api::V1::ProductsController).to receive(:authenticate_user!).and_return(nil)
+    end
+
     it "All products are displaying" do
-      get "/api/v1/products/index"
+      get "/api/v1/products"
       expect(response).to have_http_status(:success)
     end
   end
