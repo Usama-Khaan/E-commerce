956a22205f9af1d54bdbd5854a7b6c1048692993
Usama-Khaan <usama.khan@xprolabs.com>
2023-10-19 15:36:04 +0500

    Product: Product out of stock feedback incorporated

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,4 +4,8 @@ class Product < ApplicationRecord
   has_many :line_items, dependent: :destroy
   has_many :carts, through: :line_items
   validates :title, :description, :product_stock_quantity, :price, :image, presence: true
+
+  def reduce_stock_quantity(quantity)
+    update(product_stock_quantity: product_stock_quantity - quantity)
+  end
 end
