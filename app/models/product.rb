1852a7c45e258639cbb34b0af20ff7281f479350
Usama-Khaan <usama.khan@xprolabs.com>
2023-10-19 11:33:26 +0500

    Product: Image validation is added

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,5 +3,5 @@ class Product < ApplicationRecord
   has_one_attached :image
   has_many :line_items, dependent: :destroy
   has_many :carts, through: :line_items
-  validates :title, :description, :product_stock_quantity, :price, presence: true
+  validates :title, :description, :product_stock_quantity, :price, :image, presence: true
 end
